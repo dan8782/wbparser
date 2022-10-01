678b9f4606ea6b9d55f70c5222e9384cd3fae2ab
Danila Kiva <kivadan@inbox.ru>
2022-10-02 01:44:27 +0300

    init

---
+++ go.mod
@@ -0,0 +1,34 @@
+module wbparser.go
+
+go 1.19
+
+require github.com/geziyor/geziyor v0.0.0-20220429000531-738852f9321d
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/VividCortex/gohistogram v1.0.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/chromedp/cdproto v0.0.0-20220930195933-357956813629 // indirect
+	github.com/chromedp/chromedp v0.8.6 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/go-kit/kit v0.12.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2 // indirect
+	github.com/prometheus/client_golang v1.13.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.37.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
+	golang.org/x/net v0.0.0-20220930213112-107f3e3c3b0b // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220922220347-f3bd1da661af // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
---
+++ wbparser.go
@@ -0,0 +1,120 @@
+// package main
+
+// import (
+// 	//"fmt"
+// 	"log"
+// 	"github.com/PuerkitoBio/goquery"
+// 	"github.com/geziyor/geziyor"
+// 	"github.com/geziyor/geziyor/client"
+// 	//"github.com/geziyor/geziyor/export"
+// )
+
+// func main() {
+//     geziyor.NewGeziyor(&geziyor.Options{
+//         RobotsTxtDisabled: true,
+//         StartRequestsFunc: func(g *geziyor.Geziyor) {
+//             g.GetRendered("https://catalog.wb.ru/catalog/bags2/catalog?appType=1&couponsGeo=12,3,18,15,21,101&curr=rub&dest=-1029256,-51490,-184106,123585599&emp=0&lang=ru&locale=ru&pricemarginCoeff=1.0&reg=0&regions=68,64,83,4,38,80,33,70,82,86,75,30,69,1,48,22,66,31,40,71&sort=popular&spp=0&subject=50", g.Opt.ParseFunc)
+//         },
+//         ParseFunc: func(g *geziyor.Geziyor, r *client.Response) {
+//             r.HTMLDoc.Find("body").Each(func(_ int, s *goquery.Selection) {
+//                 log.Println(s.Find("div.wrapper").Text())
+//             })
+//             //fmt.Println(string(r.Body))
+//         },
+//         //BrowserEndpoint: "ws://localhost:50",
+//     }).Start()
+//     // geziyor.NewGeziyor(&geziyor.Options{
+//     //     RobotsTxtDisabled: true,
+//     //     StartURLs: []string{"https://www.wildberries.ru/catalog/aksessuary/sumki-i-ryukzaki/sumki"},
+//     //     ParseFunc: quotesParse,
+//     //     Exporters: []export.Exporter{&export.JSON{}},
+//     // }).Start()
+// }
+
+// // func quotesParse(g *geziyor.Geziyor, r *client.Response) {
+// //     fmt.Println(r.HTMLDoc.Find("body"))
+// //     r.HTMLDoc.Find("header__container").Each(func(i int, s *goquery.Selection) {
+// //         g.Exports <- map[string]interface{}{
+// //             "text":   s.Find("strong.brand-name").Text(),
+// //         }
+// //     })
+// //     if href, ok := r.HTMLDoc.Find("div.product-card-list").Attr("href"); ok {
+// //         g.Get(r.JoinURL(href), quotesParse)
+// //     }
+// // }
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Response struct {
+	State   int `json:"state"`
+	Version int `json:"version"`
+	Data    struct {
+		Products []struct {
+			Sort            int    `json:"__sort"`
+			Ksort           int    `json:"ksort"`
+			Time1           int    `json:"time1"`
+			Time2           int    `json:"time2"`
+			ID              int    `json:"id"`
+			Root            int    `json:"root"`
+			KindID          int    `json:"kindId"`
+			SubjectID       int    `json:"subjectId"`
+			SubjectParentID int    `json:"subjectParentId"`
+			Name            string `json:"name"`
+			Brand           string `json:"brand"`
+			BrandID         int    `json:"brandId"`
+			SiteBrandID     int    `json:"siteBrandId"`
+			Sale            int    `json:"sale"`
+			PriceU          int    `json:"priceU"`
+			SalePriceU      int    `json:"salePriceU"`
+			AveragePrice    int    `json:"averagePrice"`
+			Benefit         int    `json:"benefit"`
+			Pics            int    `json:"pics"`
+			Rating          int    `json:"rating"`
+			Feedbacks       int    `json:"feedbacks"`
+			Colors          []struct {
+				Name string `json:"name"`
+				ID   int    `json:"id"`
+			} `json:"colors"`
+			Sizes []struct {
+				Name     string `json:"name"`
+				OrigName string `json:"origName"`
+				Rank     int    `json:"rank"`
+				OptionID int    `json:"optionId"`
+			} `json:"sizes"`
+			DiffPrice    bool   `json:"diffPrice"`
+			PanelPromoID int    `json:"panelPromoId,omitempty"`
+			PromoTextCat string `json:"promoTextCat,omitempty"`
+			IsNew        bool   `json:"isNew,omitempty"`
+		} `json:"products"`
+	} `json:"data"`
+}
+
+func main(){
+    page:="1"
+    priceRange:="priceU=9700;100000&"
+    url :="https://catalog.wb.ru/catalog/bags2/catalog?appType=1&couponsGeo=12,3,18,15,21,101&curr=rub&dest=-1029256,-51490,-184106,123585599&emp=0&lang=ru&locale=ru&page="+page+"&"+priceRange+"pricemarginCoeff=1.0&reg=0&regions=68,64,83,4,38,80,33,70,82,86,75,30,69,1,48,22,66,31,40,71&sort=popular&spp=0&subject=50"
+    resp, err := http.Get(url)
+    if err != nil {
+        fmt.Println("No response from request")
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    var result Response
+    if err := json.Unmarshal(body, &result); err != nil { 
+        fmt.Println("Can not unmarshal JSON")
+    }
+    //fmt.Println(PrettyPrint(result.Data.Products))
+    _ = ioutil.WriteFile("test.json", []byte(PrettyPrint(result.Data.Products[0])), 0644)
+}
+
+func PrettyPrint(i interface{}) string {
+    s, _ := json.MarshalIndent(i, "", "\t")
+    return string(s)
+}
